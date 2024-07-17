f9e33efdbda0db9eb93e68e0e20ef5f4e04fb8b0
Vicky8180 <vyadav99x1@gmail.com>
2024-07-17 17:03:46 +0530

    deploy2

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,8 @@
 const mongoose=require('mongoose')
 
 
-var mongoURL="mongodb+srv://Anoop:Anoop@cluster0.swf6hyc.mongodb.net/test"
+// var mongoURL="mongodb+srv://Anoop:Anoop@cluster0.swf6hyc.mongodb.net/test"
+var mongoURL="mongodb+srv://Anoop:Anoop@cluster0.swf6hyc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
    mongoose.connect(mongoURL,{useNewUrlParser:true ,useUnifiedTopology: true,bufferCommands: false})
 var connection=mongoose.connection;
--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,25 @@
 const express = require("express")
 const app=express();
-const port = 5000
+// const port = 5000
 const dp=require("./db")
 const path  = require('path');
+const cors = require('cors')
 
 
 
 
 
+// const _dirname=path.dirname("")
+// const buildpath = path.join(_dirname,"../food-delivery/build")
+// app.use(express.static(buildpath))
 
-const _dirname=path.dirname("")
-const buildpath = path.join(_dirname,"../food-delivery/build")
-app.use(express.static(buildpath))
-
-
+app.use(cors());
 
 const FoodDetailsAPI=require("./routers/Food_Details")
 const OrderDetails= require("./routers/OrderDeails")
 
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin","https://food-delivery1-5tut.vercel.app");
     res.header("Access-Control-Allow-Headers",
     "Origin,X-Requested-With, Content-Type, Accept");
     next();
@@ -33,7 +33,8 @@ app.use('/api', OrderDetails);
 
 
 
+const PORT = process.env.PORT || 4000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
